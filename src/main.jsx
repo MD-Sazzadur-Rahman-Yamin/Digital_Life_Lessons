25c185a179e459fb301619541360e449603f0817
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-06 19:48:16 +0600

    besic file structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import router from './Routes/MainRouter.jsx';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
