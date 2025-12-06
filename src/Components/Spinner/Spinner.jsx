9052ea3f68daa705130a20402352284fc6c18dea
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-06 21:53:23 +0600

    add spinner

---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = ({ size = "loading-xl", color = "text-primary" }) => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-full">
+      <span className={`loading ${size} ${color}`}></span>;
+    </div>
+  );
+};
+
+export default Spinner;
