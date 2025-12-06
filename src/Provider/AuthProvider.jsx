bb6d59bb37eac449f668a30c76099e70e829e2b6
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-07 00:18:20 +0600

    added observer on auth provider and some state

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-import app from "../firebase/firebase.config";
+import React, { useEffect, useState } from "react";
+import app from "../firebase/firebase.init";
 import AuthContext from "./AuthContext";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const authData = {};
+  const [user, setUser] = useState(null);
+  const [userLoading, setUserLoading] = useState(true);
+
+    useEffect(() => {
+      const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setUserLoading(false);
+      });
+      return () => {
+        unSubscribe();
+      };
+    }, []);
+  const authData = { user, setUser, userLoading, setUserLoading };
   return (
     <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
   );
