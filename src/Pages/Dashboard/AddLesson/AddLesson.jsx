c5043af3348daee38276aada6819e87102b31500
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-09 20:00:10 +0600

    AddLesson route design

--- src/Pages/Dashboard/AddLesson/AddLesson.jsx
+++ src/Pages/Dashboard/AddLesson/AddLesson.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
 
 const AddLesson = () => {
-    return (
-        <div className='container'>
-            <h2>add lestion</h2>
-        </div>
-    );
+  const [category, setCategory] = useState([]);
+  const [emotionalTone, setEmotionalTone] = useState([]);
+  axios.get("/Category.json").then((result) => {
+    setCategory(result.data);
+  });
+  axios.get("/EmotionalTone.json").then((result) => {
+    setEmotionalTone(result.data);
+  });
+  return (
+    <div className="container bg-base-300 p-10! rounded-2xl min-h-screen">
+      <h2 className="font-bold! text-center">Add Lestion</h2>
+      <form>
+        <fieldset className="fieldset">
+          {/* Title */}
+          <label className="label">Lesson Title</label>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Lesson Title"
+          />
+          {/* story */}
+          <label className="label">Story</label>
+          <textarea
+            className="textarea w-full min-h-80"
+            placeholder="Story"
+          ></textarea>
+          {/* catagory */}
+          <label className="label w-full">Category</label>
+          <select defaultValue="Category" className="select w-full">
+            <option disabled={true}>Category</option>
+            {category.map((c) => (
+              <option key={c.id} value={c.options}>
+                {c.options}
+              </option>
+            ))}
+          </select>
+          {/* tone */}
+          <label className="label w-full">Emotional Tone</label>
+          <select defaultValue="Emotional Tone" className="select w-full">
+            <option disabled={true}>Emotional Tone</option>
+            {emotionalTone.map((e) => (
+              <option key={e.id} value={e.options}>
+                {e.options}
+              </option>
+            ))}
+          </select>
+          {/* Visibility */}
+          <label className="label w-full">Visibility</label>
+          <select defaultValue="Visibility" className="select w-full">
+            <option disabled={true}>Emotional Tone</option>
+            <option value="Public">Public</option>
+            <option value="Private">Private</option>
+          </select>
+          {/* Access Level */}
+          <label className="label w-full">Access Level</label>
+          <select defaultValue="Access Level" className="select w-full">
+            <option disabled={true}>Access Level</option>
+            <option value="Public">Free</option>
+            <option value="Private">Premium ⭐</option>
+          </select>
+
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default AddLesson;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import router from "./Routes/MainRouter.jsx";
 import { RouterProvider } from "react-router";
 import AuthProvider from "./Provider/AuthProvider.jsx";
