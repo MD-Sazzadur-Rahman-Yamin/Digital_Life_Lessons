f554cf716c39f96ee45452ef2e1063bc4f075ae7
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-10 00:19:19 +0600

    fetch data for login user and show a table

--- src/Pages/Dashboard/MyLessons/MyLessons.jsx
+++ src/Pages/Dashboard/MyLessons/MyLessons.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useAuth from "../../../Hooks/useAuth";
 
 const MyLessons = () => {
-    return (
-      <div className="container bg-base-300 p-10! rounded-2xl min-h-screen">
-        <h2 className="font-bold! text-center">My Lessons</h2>
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: myAllLessons = [] } = useQuery({
+    queryKey: ["my-lessons", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/lessons/my-lessons/${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(myAllLessons);
+  return (
+    <div className="container bg-base-300 p-10! rounded-2xl min-h-screen">
+      <h2 className="font-bold! text-center mb-4">My Lessons</h2>
+      <div className="overflow-x-auto rounded-box border border-base-content bg-base-300">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myAllLessons.map((lesson, index) => (
+              <tr key={lesson._id || index}>
+                <th>{index + 1}</th>
+                <td>{lesson.title}</td>
+                <td>{lesson.role || "Quality Control Specialist"}</td>
+                <td>{lesson.color || "Blue"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
+    </div>
+  );
 };
 
 export default MyLessons;
