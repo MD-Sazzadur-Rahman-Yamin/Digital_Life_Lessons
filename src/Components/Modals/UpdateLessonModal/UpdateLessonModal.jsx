0050adfecb9fcef120afc9a49c6bf1695ef0f9f2
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-10 19:02:00 +0600

    show update lesson modal and show pre-fild form

---
+++ src/Components/Modals/UpdateLessonModal/UpdateLessonModal.jsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from "react";
+import useIsPremium from "../../../Hooks/useIsPremium";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+const UpdateLessonModal = ({ modalRef, modalData }) => {
+  const [updateLessonLoading, setUpdateLessonLoading] = useState(false);
+
+  const isPremium = useIsPremium();
+  const [category, setCategory] = useState([]);
+  const [emotionalTone, setEmotionalTone] = useState([]);
+  useEffect(() => {
+    axios.get("/Category.json").then((result) => setCategory(result.data));
+    axios
+      .get("/EmotionalTone.json")
+      .then((result) => setEmotionalTone(result.data));
+  }, []);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  //add default value
+  useEffect(() => {
+    if (modalData) {
+      reset({
+        title: modalData.title || "",
+        story: modalData.story || "",
+        category: modalData.category || "",
+        emotionalTone: modalData.emotionalTone || "",
+        visibility: modalData.visibility || "",
+        accessLevel: modalData.accessLevel || "",
+      });
+    }
+  }, [modalData, reset]);
+
+
+  // frontend will sent title, story, category, emotionalTone, visibility, accessLevel,creatorEmail, createdAt, updatedAt
+  console.log(modalData);
+  const handleUpdateLesson = (data) => {
+    console.log(data);
+  };
+  return (
+    <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box">
+        <h2 className="text-center">Update Lesson</h2>
+        {/* <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl"> */}
+        <div className="card-body">
+          <form onSubmit={handleSubmit(handleUpdateLesson)}>
+            <fieldset className="fieldset">
+              {/* Title */}
+              <label className="label">Lesson Title</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Lesson Title"
+                {...register("title", { required: true })}
+              />
+              {errors.title?.type === "required" && (
+                <p className="text-red-500 text-sm">Lesson Title is required</p>
+              )}
+              {/* story */}
+              <label className="label">Story</label>
+              <textarea
+                className="textarea w-full min-h-80"
+                placeholder="Story"
+                {...register("story", { required: true })}
+              ></textarea>
+              {errors.story?.type === "required" && (
+                <p className="text-red-500 text-sm">Story is required</p>
+              )}
+              {/* catagory */}
+              <label className="label w-full">Category</label>
+              <select
+                className="select w-full"
+                {...register("category", { required: true })}
+              >
+                <option disabled={true}>Category</option>
+                {category.map((c) => (
+                  <option key={c.id} value={c.options}>
+                    {c.options}
+                  </option>
+                ))}
+              </select>
+              {errors.category?.type === "required" && (
+                <p className="text-red-500 text-sm">Category is required</p>
+              )}
+              {/* tone */}
+              <label className="label w-full">Emotional Tone</label>
+              <select
+                className="select w-full"
+                {...register("emotionalTone", { required: true })}
+              >
+                <option disabled={true}>Emotional Tone</option>
+                {emotionalTone.map((e) => (
+                  <option key={e.id} value={e.options}>
+                    {e.options}
+                  </option>
+                ))}
+              </select>
+              {errors.emotionalTone?.type === "required" && (
+                <p className="text-red-500 text-sm">
+                  Emotional Tone is required
+                </p>
+              )}
+              {/* Visibility */}
+              <label className="label w-full">Visibility</label>
+              <select
+                defaultValue="Visibility"
+                className="select w-full"
+                {...register("visibility", { required: true })}
+              >
+                <option disabled={true}>Emotional Tone</option>
+                <option value="Public">Public</option>
+                <option value="Private">Private</option>
+              </select>
+              {errors.visibility?.type === "required" && (
+                <p className="text-red-500 text-sm">Visibility is required</p>
+              )}
+              {/* Access Level */}
+              <label className="label w-full">Access Level</label>
+              <select
+                defaultValue="Access Level"
+                className="select w-full"
+                {...register("accessLevel", { required: true })}
+              >
+                <option disabled={true}>Access Level</option>
+                <option value="Public">Free</option>
+                <option disabled={!isPremium} value="Private">
+                  {isPremium
+                    ? "Premium ⭐"
+                    : "Upgrade to Premium to create paid lessons"}
+                </option>
+              </select>
+              {errors.accessLevel?.type === "required" && (
+                <p className="text-red-500 text-sm">Access Level is required</p>
+              )}
+
+              <button
+                className="btn btn-primary mt-4"
+                disabled={updateLessonLoading}
+              >
+                {updateLessonLoading ? (
+                  <span className="loading loading-spinner"></span>
+                ) : (
+                  "Add Lesson"
+                )}
+              </button>
+            </fieldset>
+          </form>
+        </div>
+        {/* </div> */}
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-secondary">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default UpdateLessonModal;
--- src/Pages/Dashboard/MyLessons/MyLessons.jsx
+++ src/Pages/Dashboard/MyLessons/MyLessons.jsx
@@ -4,10 +4,12 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useAuth from "../../../Hooks/useAuth";
 import { Link } from "react-router";
 import SeeStatsModal from "../../../Components/Modals/SeeStatsModal/SeeStatsModal";
+import UpdateLessonModal from "../../../Components/Modals/UpdateLessonModal/UpdateLessonModal";
 
 const MyLessons = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
+
   const { data: myAllLessons = [] } = useQuery({
     queryKey: ["my-lessons", user?.email],
     enabled: !!user?.email,
@@ -16,21 +18,28 @@ const MyLessons = () => {
       return res.data;
     },
   });
-  console.log(myAllLessons);
 
-  //see states modals
+  // Stats modal
   const seeStatsModalRef = useRef();
-  const [seeStatsModalData, setSeeStatsModalData] = useState({});
-  const OpenSeeStatsModal = (lesson) => {
+  const [seeStatsModalData, setSeeStatsModalData] = useState(null);
+  const openSeeStatsModal = (lesson) => {
     setSeeStatsModalData(lesson);
-    seeStatsModalRef.current.showModal();
+    seeStatsModalRef.current?.showModal();
   };
+
+  // Update lesson modal
+  const updateLessonModalRef = useRef();
+  const [updateLessonModalData, setUpdateLessonModalData] = useState(null);
+  const openUpdateLessonModal = (lesson) => {
+    setUpdateLessonModalData(lesson);
+    updateLessonModalRef.current?.showModal();
+  };
+
   return (
-    <div className="container bg-base-300 p-10! rounded-2xl min-h-screen">
-      <h2 className="font-bold! text-center mb-4">My Lessons</h2>
+    <div className="container bg-base-300 p-10 rounded-2xl min-h-screen">
+      <h2 className="font-bold text-center mb-4">My Lessons</h2>
       <div className="overflow-x-auto rounded-box border border-base-content bg-base-300">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th className="text-center">#</th>
@@ -53,21 +62,33 @@ const MyLessons = () => {
                 <td className="text-center">
                   <button
                     className="btn btn-primary"
-                    onClick={() => OpenSeeStatsModal(lesson)}
+                    onClick={() => openSeeStatsModal(lesson)}
                   >
                     See Stats
                   </button>
                 </td>
-                <td className="text-center">{lesson.color || "Blue"}</td>
+                <td className="text-center">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => openUpdateLessonModal(lesson)}
+                  >
+                    Update
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
       <SeeStatsModal
         modalRef={seeStatsModalRef}
         modalData={seeStatsModalData}
-      ></SeeStatsModal>
+      />
+      <UpdateLessonModal
+        modalRef={updateLessonModalRef}
+        modalData={updateLessonModalData}
+      />
     </div>
   );
 };
